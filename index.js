877801e5275c15370093a7434b5ec76a8a235e43
zerofata <fxsliam@outlook.com>
2025-07-14 19:29:29 +1200

    Update index.js

--- index.js
+++ index.js
@@ -86,32 +86,28 @@ async function getCharStyleString(stChar) {
     const dialogueColor = await getCharacterDialogueColor(stChar);
 
     if (dialogueColor) {
-        // For character messages
         styleHtml += `
             .mes[xdc-author_uid="${stChar.uid}"] {
                 --character-color: #${dialogueColor.toHex()};
             }
         `;
 
-        // For user messages
         styleHtml += `
             .mes[is_user="true"] {
-                --character-color: YOUR_DESIRED_USER_COLOR;
+                --character-color: #${dialogueColor.toHex()};
             }
         `;
 
         if (extSettings.colorizeTargets & ColorizeTargetType.QUOTED_TEXT) {
             styleHtml += `
-                .mes[xdc-author_uid="${stChar.uid}"] .mes_text q,
-                .mes[is_user="true"] .mes_text q {
+                .mes[xdc-author_uid="${stChar.uid}"] .mes_text q {
                     color: var(--character-color);
                 }
             `;
         }
         if (extSettings.colorizeTargets & ColorizeTargetType.BUBBLES) {
             styleHtml += `
-                .bubblechat .mes[xdc-author_uid="${stChar.uid}"],
-                .bubblechat .mes[is_user="true"] {
+                .bubblechat .mes[xdc-author_uid="${stChar.uid}"] {
                     background-color: var(--character-color) !important;
                     border-color: var(--character-color) !important;
                 }
